2867c42532d91af26da85ac0decd1e51d86f462e
berkayoruc <berkayoruc@hotmail.com>
2020-06-27 21:10:03 +0300

    add new field and modify field

--- README.md
+++ README.md
@@ -4,14 +4,16 @@ Auto Numbering plugin
 Description
 -----------
 
-This plugin creates a new field that contains the order of features, after sorting by selected criteria.
+This plugin creates a new field or modify selected field that contains the order of features, after sorting by selected criteria.
 
 UI
 --
 
-First, select the vector layer you want to number. Then, select the fields you want to order. Default order is ascending. Check the box next to a field to choose a descending order.  Finally, enter the name of the numbering field to create. Default is 'idorder'.
+First, select the vector layer which you want to create new sorted field or modify exist field. Then, select the field you want to order. Default order is ascending. You can create new field or modify exist field. If you want to create new field, type field name. If you want to modify exist field, select field from field list. Finally, click run button and new or modified field is ready.
 
 
 Changelog
 ---------
-
+Version 0.1 - 27 June 2020
+- Add new field
+- Modify exist field
--- autoNumberingDialog.py
+++ autoNumberingDialog.py
@@ -1,11 +1,17 @@
+import os.path
+
 from qgis.PyQt.QtCore import Qt
-from qgis.PyQt.QtWidgets import (QComboBox, QDialog, QFormLayout, QLabel,
-                                 QSizePolicy, QSpacerItem)
+from qgis.PyQt.QtGui import QIcon
+from qgis.PyQt.QtWidgets import (QCheckBox, QComboBox, QDialog, QFormLayout,
+                                 QHBoxLayout, QLabel, QPushButton, QSizePolicy,
+                                 QSpacerItem, QSizePolicy, QDialogButtonBox, QLineEdit, QRadioButton)
 
 
 class AutoNumberingDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.setWindowTitle("Auto Numbering")
+        self.setWindowIcon(QIcon(os.path.dirname(__file__)+'/icon.png'))
         self.resize(300, 300)
         self.formLayout = QFormLayout(self)
         self.formLayout.setSpacing(12)
@@ -20,11 +26,37 @@ class AutoNumberingDialog(QDialog):
         self.formLayout.setWidget(0, QFormLayout.FieldRole, self.layerCB)
         self.label1 = QLabel(self)
         self.label1.setObjectName("label1")
-        self.label1.setText("Field")
+        self.label1.setText("Base Field")
         self.formLayout.setWidget(1, QFormLayout.LabelRole, self.label1)
         self.fieldCB = QComboBox(self)
         self.fieldCB.setObjectName("fieldCB")
         self.formLayout.setWidget(1, QFormLayout.FieldRole, self.fieldCB)
+        self.rButton1 = QRadioButton("Modify exist field")
+        self.rButton1.setObjectName("rButton1")
+        self.formLayout.setWidget(3, QFormLayout.LabelRole, self.rButton1)
+        self.fieldCB1 = QComboBox(self)
+        self.fieldCB1.setObjectName("fieldCB1")
+        self.fieldCB1.setEnabled(False)
+        self.formLayout.setWidget(3, QFormLayout.FieldRole, self.fieldCB1)
+        self.rButton2 = QRadioButton("Add new field")
+        self.rButton2.setObjectName("rButton2")
+        self.formLayout.setWidget(2, QFormLayout.LabelRole, self.rButton2)
+        self.newField = QLineEdit(self)
+        self.newField.setObjectName("newField")
+        self.newField.setPlaceholderText("Type new field name")
+        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.newField)
+        horizontalSpacer = QSpacerItem(40,20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        self.formLayout.addItem(horizontalSpacer)
+        self.buttonBox = QDialogButtonBox(self)
+        self.buttonBox.setOrientation(Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QDialogButtonBox.Cancel|QDialogButtonBox.Ok)
+        self.buttonBox.button(QDialogButtonBox.Ok).setText("Run")
+        self.buttonBox.setObjectName("buttonBox")
+        self.formLayout.setWidget(5, QFormLayout.FieldRole, self.buttonBox)
+
+        
+        
 
         verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)    # vertical spacer
         self.formLayout.addItem(verticalSpacer)
+        self.setSizePolicy(QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum))
